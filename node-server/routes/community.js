a1f5d2a21f6e1ae900ce1409d1fc51cfe6112031
PollyGotACracker <bitterns96@gmail.com>
2023-01-25 16:21:22 +0900

    게시글 insert 구현

---
+++ node-server/routes/community.js
@@ -0,0 +1,55 @@
+import express from "express";
+import fileUp from "../modules/file_upload.js";
+import DB from "../models/index.js";
+import fs from "fs";
+import path from "path";
+import { v4 } from "uuid";
+
+const BOARD = DB.models.board_content;
+const ATTACH = DB.models.attach;
+
+const router = express.Router();
+
+// editor 에 이미지 업로드
+// fileUp.single("...") : formData 객체에 file 을 append 했던 key 값으로 지정
+// (key=value 로 저장되므로 input tag 의 name 과 동일한 역할)
+// 파일을 여러 개 선택하더라도 별도로 요청하므로 single 로 받을 것
+router.post("/upload", fileUp.single("upload"), async (req, res, next) => {
+  console.log("file", req.file);
+  // 게시글 uuid
+  console.log("code", req.body.bcode);
+
+  try {
+    const file = req.file;
+    const bcode = req.body.bcode;
+    const uploadFileInfo = {
+      a_code: v4(),
+      b_code: bcode,
+      a_original_name: file.originalname,
+      a_save_name: file.filename,
+      a_ext: file.mimetype,
+    };
+    await ATTACH.create(uploadFileInfo);
+
+    return res.json({
+      uploaded: true,
+      url: uploadFileInfo.a_save_name,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.post("/post/insert", async (req, res, next) => {
+  const data = req.body;
+  console.log(data);
+  try {
+    await BOARD.create(data);
+
+    return res.send({ MESSAGE: "POST INSERT" });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+export default router;
--- react-client/src/comp/community/Write.js
+++ react-client/src/comp/community/Write.js
@@ -4,7 +4,9 @@
 
 import { useEffect, useState } from "react";
 // react build 전까지 에디터 보류
-// import Editor from "./EditorModule";
+// import EditorModule from "./EditorModule";
+import { submitPost } from "../service/post.service";
+import { v4 } from "uuid";
 
 const Write = () => {
   const catList = [
@@ -16,18 +18,11 @@ const Write = () => {
 
   const initPost = () => {
     const postData = {
-      b_code: "",
-      username: "",
+      b_code: v4(),
+      username: "polly@gmail.com",
       b_title: "",
       b_content: "",
       b_category: "",
-      b_date: "",
-      b_time: "",
-      b_update: "",
-      b_delete: "",
-      b_views: "",
-      b_upvote: "",
-      b_group: "",
     };
     return postData;
   };
@@ -40,7 +35,11 @@ const Write = () => {
 
   const onChangeContentHandler = (e, editor) => {
     const data = editor.getData();
-    setPostData({ ...postData, [e.target.name]: data });
+    setPostData({ ...postData, b_content: data });
+  };
+
+  const onClickHandler = (e) => {
+    submitPost(postData);
   };
 
   useEffect(() => {
@@ -70,18 +69,14 @@ const Write = () => {
         {checkboxList}
       </div>
       <input
-        className="b_title"
+        className="title"
         name="b_title"
         placeholder="제목"
         value={postData.b_title}
         onChange={onChangeHandler}
       />
-      {/* <Editor
-        className="b_content"
-        name="b_content"
-        onChangeHandler={onChangeContentHandler}
-      /> */}
-      <button id="submit" type="button">
+      {/* <EditorModule handler={onChangeContentHandler} b_code={postData.b_code} /> */}
+      <button id="submit" type="button" onClick={onClickHandler}>
         등록
       </button>
     </form>
