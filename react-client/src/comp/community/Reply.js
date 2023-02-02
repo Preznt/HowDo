4d77d1b3951abd42737706fa15cd0611719f78b3
PollyGotACracker <bitterns96@gmail.com>
2023-02-02 18:29:17 +0900

    댓글 컴포넌트

--- react-client/src/comp/community/Reply.js
+++ react-client/src/comp/community/Reply.js
@@ -1,21 +1,35 @@
-import ReplyItem from "./ReplyItem";
+import ReplyList from "./ReplyList";
 
-const Reply = ({ data }) => {
-  const ReplyList = () => {
-    return data
-      .filter((item) => {
-        return !item.r_parent_code;
-      })
-      .map((item) => {
-        return <ReplyItem key={item.r_code} data={data} item={item} />;
-      });
-  };
+const Input = ({
+  replyCount,
+  replyData,
+  replyList,
+  onChangeHandler,
+  onClickReply,
+}) => {
+  const btnClass02 =
+    "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded";
+  const inputClass =
+    "bg-transparent border-b border-blue-700 flex-1 mr-3 py-1 px-2 leading-tight focus:outline-none";
 
   return (
-    <section className="reply">
-      <ReplyList />
+    <section className="m-5">
+      <div className="text-lg">{`댓글 ${replyCount} 개`}</div>
+      <div className="reply-input-box flex mt-5 mb-5 p-10 w-full border border-gray-300 rounded">
+        {/* 사용자 nickname 필요 */}
+        <input
+          className={inputClass}
+          type="text"
+          value={replyData.r_content}
+          onChange={onChangeHandler}
+        />
+        <button className={btnClass02} onClick={onClickReply}>
+          등록
+        </button>
+      </div>
+      <ReplyList data={replyList} />
     </section>
   );
 };
 
-export default Reply;
+export default Input;
