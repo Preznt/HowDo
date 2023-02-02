853b4517331c96dfc5b1d74baa821963913cc98a
SoonhanSaram <kyengmin911@naver.com>
2023-02-03 01:32:25 +0900

    0203 슬라이드 커스터마이징

--- react-client/src/comp/mypage/CreaterContent.js
+++ react-client/src/comp/mypage/CreaterContent.js
@@ -7,7 +7,7 @@ import { useVideoContentContext } from "../../context/VideoContentContextProvide
  */
 const CreaterContent = () => {
   const { videoContentList, setVideoContentList } = useVideoContentContext();
-
+  const { position, setPosition } = useState(0);
   const setHover = (v_code, toggle) => {
     setVideoContentList([
       ...videoContentList.map((item) => {
@@ -33,10 +33,31 @@ const CreaterContent = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+  const CONTENT_WIDTH = 256;
+  const before = () => {
+    let newPosition = position + CONTENT_WIDTH;
+    console.log(CONTENT_WIDTH * (videoContentList.length - 1) * -1);
+    console.log(position);
+    if (position === 0) {
+      newPosition = CONTENT_WIDTH * (videoContentList.length - 1) * -1;
+    }
+    console.log(newPosition);
+    setPosition(newPosition);
+
+    console.log(position);
+  };
+
+  const next = () => {
+    let newPosition = position + CONTENT_WIDTH;
+    if (position === 0) {
+      newPosition = CONTENT_WIDTH * (videoContentList.length - 1) * -1;
+    }
+    setPosition(newPosition);
+  };
   const videoView = videoContentList?.map((item) => {
     return (
       <div
-        className="m-12 flex w-80 h-64 flex-col justify-center items-center transition-all duration-700 hover:h-72 hover:w-96 hover:ml-4 hover:mr-4 hover:mb-4 shadow-lg"
+        className="m-12 flex w-80 h-52 flex-col justify-center items-center transition-all duration-700 hover:h-64 hover:w-96 hover:ml-4 hover:mr-4 hover:mb-4 shadow-lg"
         onMouseOver={() => onMouseOverHandler(item.v_code)}
         onMouseOut={() => onMouseOutHandler(item.v_code)}
         key={item.v_code}
@@ -59,11 +80,20 @@ const CreaterContent = () => {
   });
 
   return (
-    <div className="w-full text-center min-h-64">
-      <span className="p-4  border-b-2 border-black text-center">
+    <div className="w-full text-center min-h-80">
+      <span className="p-4 border-b-2 border-black text-center">
         최근 업로드한 영상
       </span>
-      <div className="flex">{videoView}</div>
+      <div
+        className="flex overflow-hidden relative min-h-80 w-full"
+        style={{ transform: `translateX(${position}px)` }}
+      >
+        <div className="absolute flex w-full h-full top-0 left-0 transition-all duration-700">
+          {videoView}
+          <div onClick={before}>앞</div>
+          <div onClick={next}>뒤</div>
+        </div>
+      </div>
     </div>
   );
 };
