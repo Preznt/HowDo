1e1c62de9296ca7f5fe97c445f30324f1581d59d
SoonhanSaram <kyengmin911@naver.com>
2023-02-05 18:50:47 +0900

    0205 mypage 점검

--- react-client/src/comp/mypage/CreaterContentFavorite.js
+++ react-client/src/comp/mypage/CreaterContentFavorite.js
@@ -1,39 +1,95 @@
 import ReactPlayer from "react-player";
 import { useState } from "react";
+import { useVideoContentContext } from "../../context/VideoContentContextProvide";
+import {
+  moreButton,
+  myPageContentMain,
+  nameLabel,
+  videoBeforeButton,
+  videoContenView,
+  videoNextButton,
+  videoNohover,
+} from "../../nav/classNames/ClassNames";
+
 /**
  * map 을 이용한 컨텐츠 시리즈별 carousel 제작
  */
 const CreaterContentFavorite = () => {
-  const [hover, setHover] = useState(false);
-  console.log(hover);
-  return (
-    <div className="w-full text-center min-h-64">
-      <span className="p-4 border-b-2 border-black">인기 업로드 영상</span>
+  const { videoContentList, setVideoContentList } = useVideoContentContext();
+  const [position, setPosition] = useState(0);
+  const CONTENT_WIDTH = 392;
+
+  const setHover = (v_code, toggle) => {
+    setVideoContentList([
+      ...videoContentList.map((item) => {
+        if (item.v_code === v_code) return { ...item, v_hover: toggle };
+        else return item;
+      }),
+    ]);
+    console.log(videoContentList);
+  };
+  const onMouseOverHandler = (v_code) => {
+    setHover(v_code, true);
+  };
+  const onMouseOutHandler = (v_code) => {
+    setHover(v_code, false);
+  };
+
+  const before = () => {
+    let newPosition = position - CONTENT_WIDTH;
+    if (position <= (CONTENT_WIDTH * videoContentList.length - 1) * -1) {
+      newPosition = 0;
+    }
+    setPosition(newPosition);
+  };
+
+  const next = () => {
+    let newPosition = position + CONTENT_WIDTH;
+    if (position === 0) {
+      newPosition = CONTENT_WIDTH * (videoContentList.length - 1) * -1;
+    }
+    setPosition(newPosition);
+  };
+  const videoView = videoContentList?.map((item) => {
+    return (
       <div
-        className="m-12 flex w-80 h-64 flex-col justify-center items-center transition-all duration-700 hover:h-72 hover:w-96 hover:ml-4 hover:mr-4 hover:mb-4 shadow-lg"
-        onMouseOver={() => setHover(true)}
-        onMouseOut={() => setHover(false)}
+        className={videoNohover}
+        onMouseOver={() => onMouseOverHandler(item.v_code)}
+        onMouseOut={() => onMouseOutHandler(item.v_code)}
+        key={item.v_code}
       >
-        {hover ? (
-          <ReactPlayer
-            width="100%"
-            url="https://www.youtube.com/embed/1hcdQixxJdA"
-            playing={true}
-            muted={true}
-          />
-        ) : (
-          <ReactPlayer
-            width="100%"
-            url="https://www.youtube.com/embed/1hcdQixxJdA"
-            playing={false}
-            muted={false}
-          />
-        )}
+        <ReactPlayer
+          width="100%"
+          url={item.v_src}
+          playing={item.v_hover}
+          muted={item.v_hover}
+        />
+        ;
         <div>
           <img alt="profile"></img>
-          <h3>영상제목1</h3>
-          <h6>작성자</h6>
-          <h6>조회수</h6>
+          <h3>{item.v_title}</h3>
+          <h6>{item.username}</h6>
+          <h6>{item.v_count}</h6>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className={myPageContentMain}>
+      <span className={nameLabel}>최근 업로드한 영상</span>
+      <div className={videoNextButton} onClick={before}>
+        앞
+      </div>
+      <div className={videoBeforeButton} onClick={next}>
+        뒤
+      </div>
+      <div
+        className={videoContenView}
+        style={{ transform: `translateX(${position}px)` }}
+      >
+        <div className="flex">
+          {videoView},<div className={moreButton}>더보기</div>
         </div>
       </div>
     </div>
