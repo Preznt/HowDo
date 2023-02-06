b3f7e88c25aea0cba2bea3cad40718d137d57828
PollyGotACracker <bitterns96@gmail.com>
2023-02-06 12:58:24 +0900

    게시판 레이아웃 수정

--- node-server/react-client/src/comp/community/Board.js
+++ node-server/react-client/src/comp/community/Board.js
@@ -4,6 +4,8 @@ import "../../css/community/Board.css";
 import { getBoardPosts } from "../../service/post.service";
 import { useLoaderData, Link } from "react-router-dom";
 import { useUserContext } from "../../context/UserContextProvider";
+import { BarsArrowDownIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 export const loader = async ({ params }) => {
   const bEng = params.board;
@@ -14,34 +16,70 @@ export const loader = async ({ params }) => {
 const Board = () => {
   const { userSession } = useUserContext();
   const { data, board } = useLoaderData();
+  const orderList = [
+    { o_eng: "latest", o_kor: "최신순" },
+    { o_eng: "upvotes", o_kor: "추천순" },
+    { o_eng: "replies", o_kor: "댓글순" },
+    { o_eng: "views", o_kor: "조회순" },
+  ];
+  const [showOrder, setShowOrder] = useState(false);
+  const [orderValue, setOrderValue] = useState(`${orderList[0].o_kor}`);
+
+  const onClickSetOrder = (value) => {
+    setOrderValue(value);
+  };
+
+  const onClickShowOrder = () => {
+    setShowOrder(!showOrder);
+  };
 
   const btnClass =
     "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
+  const btnClass02 =
+    "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded";
   const selectClass =
     "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:outline-none focus:ring-blue-500 focus:border-blue-500 p-2";
+  const optionClass = "w-full p-2 hover:bg-blue-500 hover:text-white";
   const inputClass =
     "bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 p-2";
 
   return (
     <main className="commu-cat w-full">
-      <h1>{board.b_kor}</h1>
+      <h1 className="text-xl font-bold p-5">{board.b_kor}</h1>
       <section className="flex w-full pl-5 pr-5 pb-10 justify-between">
-        <button className={`search-select ${selectClass}`}>{"최신순"}</button>
-        <div className="hidden">
-          <button className="latest">최신순</button>
-          <button className="upvote">추천순</button>
-          <button className="reply">댓글순</button>
-          <button className="views">조회순</button>
-        </div>
+        <button
+          className={`search-select relative w-30 ${selectClass}`}
+          onClick={onClickShowOrder}
+        >
+          <BarsArrowDownIcon className="inline-block mr-3 h-5 w-5 text-slate-500" />
+          {orderValue}
+          <div
+            className="flex flex-col absolute top-11 left-0 w-full bg-gray-50 rounded border border-gray-300 text-gray-900"
+            style={{ display: showOrder === true ? "flex" : "none" }}
+          >
+            {orderList.map((order) => {
+              return (
+                <checkbox
+                  className={`${optionClass}`}
+                  name="order"
+                  value={order.o_eng}
+                  onClick={() => onClickSetOrder(order.o_kor)}
+                >
+                  {order.o_kor}
+                </checkbox>
+              );
+            })}
+          </div>
+        </button>
         <div className="cat-search flex justify-center">
           <input className={inputClass} />
-          <button>검색</button>
+          <button className={`ml-2 ${btnClass02}`}>검색</button>
         </div>
         {/* 로그인 유저의 등급이 게시판 권한등급보다 같거나 높을 때 */}
         {Number(userSession?.level) >= Number(board.b_level) && (
           <Link
             className={btnClass}
-            to={`/community/write`}
+            to={`/community/${board.b_eng}/write`}
             state={{
               b_code: board.b_code,
               b_eng: board.b_eng,
--- node-server/react-client/src/comp/community/PostWrite.js
+++ node-server/react-client/src/comp/community/PostWrite.js
@@ -3,25 +3,25 @@ import "../../css/community/Content.css";
 import { submitPost } from "../../service/post.service";
 import { usePostContext } from "../../context/PostContextProvider";
 import { useUserContext } from "../../context/UserContextProvider";
-
 import { useLayoutEffect } from "react";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 
 const PostWrite = () => {
-  const { userSession } = useUserContext();
-
   const nav = useNavigate();
+  const { userSession } = useUserContext();
   const { initPost, postData, setPostData } = usePostContext();
   const location = useLocation();
   const { b_code, b_eng, b_group_code } = location?.state;
   const data = location?.state?.data;
   const pCode = useParams().post;
 
+  const btnClass =
+    "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
+
   useLayoutEffect(() => {
     // setState 를 같은 함수 내에서 여러 번 실행하면
     // 가장 마지막 setState 만 실행된다.
 
-    // username 추가 필요
     // insert
     if (!pCode) {
       const init = initPost();
@@ -61,7 +61,7 @@ const PostWrite = () => {
   return (
     <form className="post-editor">
       <input
-        className="title"
+        className="title w-full p-1 pl-2 mb-2 border border-[#ccced1] focus:outline-none focus:border-[#2977ff]"
         name="p_title"
         placeholder="제목"
         value={postData.p_title}
@@ -72,7 +72,19 @@ const PostWrite = () => {
         handler={onChangeContentHandler}
         code={postData.p_code}
       />
-      <button id="submit" type="button" onClick={onClickHandler}>
+      <section className="write-thumb mt-2 p-2 pl-4 w-full border border-[#ccced1]">
+        <span className="select-none">대표 이미지</span>
+        <div className="thumb-select w-full h-40"></div>
+      </section>
+      <button
+        id="submit"
+        className={`m-6 float-right ${btnClass}`}
+        type="button"
+        onClick={onClickHandler}
+        disabled={
+          postData.p_title.length < 0 || postData.p_content < 0 ? true : false
+        }
+      >
         등록
       </button>
     </form>
