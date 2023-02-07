3c5e41826f9849affcb9b379832769d95872ad87
SoonhanSaram <kyengmin911@naver.com>
2023-02-07 10:23:40 +0900

    0207 발표전 수정

---
+++ node-server/react-client/src/comp/serachPage/SearchItem.js
@@ -0,0 +1,8 @@
+import { useLoaderData, useNavigate } from "react-router-dom";
+
+const SearchItem = () => {
+  const navigate = useNavigate();
+  const SearchR = useLoaderData();
+};
+
+export default SearchItem;
