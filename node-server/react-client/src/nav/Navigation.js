ceef05c771d58fb434d050d4afb53a7f4377ecc6
SoonhanSaram <kyengmin911@naver.com>
2023-02-07 14:02:15 +0900

    0207777

--- node-server/react-client/src/nav/Navigation.js
+++ node-server/react-client/src/nav/Navigation.js
@@ -17,11 +17,11 @@ import PostWrite from "../comp/community/PostWrite";
 import Approve from "../comp/purchase/Approve";
 import SearchMain, { SearchLoader } from "../comp/serachPage/SearchMain";
 import UserPageMain, { userPageFetch } from "../comp/userpage/UserPageMain";
-import ShortsVideo from "../comp/Video/ShortsVideo";
 import VideoDetail from "../comp/Video/VideoDetail";
 import CreaterMain, {
   loader as CreaterFetch,
 } from "../comp/userpage/CreaterMain";
+import ShortsMain from "../comp/Video/Main";
 
 const router = createBrowserRouter([
   {
@@ -60,7 +60,7 @@ const router = createBrowserRouter([
       },
       { path: "/approval", element: <Approve /> },
       { path: "/search/:query", loader: SearchLoader, element: <SearchMain /> },
-      { path: "/video/shorts", element: <ShortsVideo /> },
+      { path: "/video/shorts", element: <ShortsMain /> },
       { path: "/video/detail/:v_code", element: <VideoDetail /> },
     ],
   },
